13199ba38ace1a5be498b7d3b097a2e8f85d3a7d
Z4mbi3 <mikevancamp@outlook.com>
2017-10-27 21:40:49 +0200

    "Minor" fixes

--- Projects/Test plugin/src/xyz/yetimountain/Z4mbi3/Main.java
+++ Projects/Test plugin/src/xyz/yetimountain/Z4mbi3/Main.java
@@ -1,34 +1,31 @@
 package xyz.yetimountain.Z4mbi3;
 
 
-
-
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin{
 
-		@Override
-		public void onEnable() 
-		{
-			System.out.println("Plugin startup successful");
-		}
-		
-		@Override
-		public void onDisable() 
-		{
-			System.out.println("Plugin shutting down..");
-		}
-		
-		@EventHandler
-		public void onPlayerJoinEvent(PlayerJoinEvent event)
-		{
-			if(!event.getPlayer().hasPlayedBefore()) 
-			{
-				event.getPlayer().sendMessage(ChatColor.AQUA + "Welcome to the server!");
-			}
-		}
+public class Main extends JavaPlugin implements Listener{
+	
+	public Main plugin;
+	
+	public void onEnable() {
+        getConfig().options().copyDefaults(true);
+getServer().getPluginManager().registerEvents(this,this);
+        saveConfig();
+    }
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event)
+	{
+		Player player = event.getPlayer();
+		event.setJoinMessage("Test hehe" + player.getName());
+		Bukkit.broadcastMessage(ChatColor.AQUA + "Test");
+	}
 		
 }

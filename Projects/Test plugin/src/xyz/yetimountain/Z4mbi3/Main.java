d82217f5d4176b82a0109e90270817e408880265
Z4mbi3 <mikevancamp@outlook.com>
2017-10-27 15:39:00 +0200

    Added test plugin

---
+++ Projects/Test plugin/src/xyz/yetimountain/Z4mbi3/Main.java
@@ -0,0 +1,36 @@
+package xyz.yetimountain.Z4mbi3;
+
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+		@Override
+		public void onEnable() 
+		{
+			Bukkit.getLogger().log(Level.INFO, "Plugin test works");
+		}
+		
+		@Override
+		public void onDisable() 
+		{
+			Bukkit.getLogger().log(Level.INFO, "Plugin does not work");
+		}
+		
+		@EventHandler
+		public void onPlayerJoin(PlayerJoinEvent event)
+		{
+			Player player = event.getPlayer();
+			player.sendMessage(ChatColor.AQUA + "Welcome to the server!\\nType /info for the overview of all commands.");
+			
+		}
+		
+}

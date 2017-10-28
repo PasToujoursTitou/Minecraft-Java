fd57d293f301b17d3a50d316ec2058b971d336a8
Z4mbi3 <mikevancamp@outlook.com>
2017-10-28 17:55:28 +0200

    Fully updated

--- Projects/SpawnCommand/src/xyz/yetimountain/Z4mbi3/Main.java
+++ Projects/SpawnCommand/src/xyz/yetimountain/Z4mbi3/Main.java
@@ -1,5 +1,71 @@
 package xyz.yetimountain.Z4mbi3;
 
-public class Main {
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
-}
+public class Main extends JavaPlugin implements Listener
+{
+	public Main plugin;
+	
+	public void onEnable()
+	{
+		System.out.println("Plugin successfully loaded!");
+		getConfig().options().copyDefaults();
+		saveConfig();
+	}
+	public void onDisable()
+	{
+		System.out.println("Plugin shutting down...");
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String Label, String[] args)
+	{
+		//For all commands
+		Player player = (Player) sender;
+		int x = (int) player.getLocation().getX();
+		int y = (int) player.getLocation().getY();
+		int z = (int) player.getLocation().getZ();
+		float pitch = player.getLocation().getPitch();
+		float yaw = player.getLocation().getYaw();
+		World world = player.getWorld();
+		
+		if(cmd.getName().equalsIgnoreCase("setspawn"))
+		{		
+			
+			if(player.isOp())
+			{			
+				//Get location of player:
+
+				world.setSpawnLocation(x, y + 1, z);
+				world.getSpawnLocation().setPitch(pitch);
+				world.getSpawnLocation().setYaw(yaw);
+				
+				player.sendMessage(ChatColor.LIGHT_PURPLE + "Spawn has been set to " + x + " " + y + " " + z +".");
+			}
+			else
+			{
+				player.sendMessage(ChatColor.RED + "You have no permissions to use this command!");
+			}
+			return true;
+		}
+		if(cmd.getName().equalsIgnoreCase("spawn"))
+		{
+			if(world.getSpawnLocation() == null)
+			{
+				player.sendMessage(ChatColor.RED + "Spawn has not been set yet.");
+			}
+			else
+			{
+				player.teleport(world.getSpawnLocation());
+			}
+			return true;
+		}
+		return false;
+	}
+	}

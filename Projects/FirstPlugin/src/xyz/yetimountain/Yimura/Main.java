3da1a0d8f5321df9dbaa14a85407738d4ec01f1f
Yimura <andreas.maerten@scarlet.be>
2017-10-28 10:42:27 +0200

    Fixes which shoukd prevent bugs from occuring

--- Projects/FirstPlugin/src/xyz/yetimountain/Yimura/Main.java
+++ Projects/FirstPlugin/src/xyz/yetimountain/Yimura/Main.java
@@ -1,8 +1,5 @@
 package xyz.yetimountain.Yimura;
 
-import java.util.logging.Level;
-
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,12 +9,12 @@ public class Main extends JavaPlugin
 	@Override
 	public void onEnable()
 	{
-		Bukkit.getLogger().log(Level.INFO, "Plugin Gamemode abbravation has been loaded!");
+		this.getLogger().info("Plugin fully loaded!");
 	}
 	@Override
 	public void onDisable() 
 	{
-		Bukkit.getLogger().log(Level.INFO, "Plugin Gamemode abbravation has been unloaded!");
+		this.getLogger().info("Plugin unloaded!");
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)

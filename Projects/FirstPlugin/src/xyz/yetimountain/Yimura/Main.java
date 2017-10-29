321d80b433a7412eba754491ce110f5dc17b9182
Yimura <andreas.maerten@scarlet.be>
2017-10-29 23:00:03 +0100

    Fixes and final additions

--- Projects/FirstPlugin/src/xyz/yetimountain/Yimura/Main.java
+++ Projects/FirstPlugin/src/xyz/yetimountain/Yimura/Main.java
@@ -1,7 +1,9 @@
-package xyz.yetimountain.Yimura.shortGameMode;
+package xyz.yetimountain.Yimura;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin
@@ -15,44 +17,50 @@ public class Main extends JavaPlugin
 	@Override
 	public void onDisable() 
 	{
-		this.getLogger().info("Plugin unloaded!");
+		this.getLogger().info("Plugin has shut down!");
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
-		Player player = (Player) sender
+		Player player = (Player) sender;
 		if(sender instanceof Player)
 		{
-			if(cmd.getName().equalsIgnoreCases("gm"))
+			if(cmd.getName().equalsIgnoreCase("gm"))
 			{
 				if(args.length == 1)
 				{
-					if(args[0].equalsIgnoreCases("0"))
+					if(args[0].equalsIgnoreCase("0"))
 					{
 						player.performCommand("gamemode 0");
+						return true;
 					}
-					else if(args[0].equalsIgnoreCases("1"))
+					else if(args[0].equalsIgnoreCase("1"))
 					{
 						player.performCommand("gamemode 1");
+						return true;
 					}
-					else if(args[0].equalsIgnoreCases("2"))
+					else if(args[0].equalsIgnoreCase("2"))
 					{
 						player.performCommand("gamemode 2");
+						return true;
 					}
-					else if(args[0].equalsIgnoreCases("3"))
+					else if(args[0].equalsIgnoreCase("3"))
 					{
 						player.performCommand("gamemode 3");
+						return true;
 					}
 				}
 				else
 				{
-					sender.sendMessage('Usage: "gm <0/1/2/3>".');
+					sender.sendMessage(ChatColor.RED"Usage: \"gm <0/1/2/3>\".");
+					return true;
 				}
 			}
 		}
 		else
 		{
-			sender.sendMessage("Console can not run this command!");
+			sender.sendMessage("Console cannot run this command!");
+			return false;
 		}
 	}
 }

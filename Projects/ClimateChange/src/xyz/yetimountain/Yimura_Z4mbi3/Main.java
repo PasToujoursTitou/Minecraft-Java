448965363b57e44e6c952377f78d568c59f5e71a
Yimura <andreas.maerten@scarlet.be>
2017-10-31 00:01:52 +0100

    Added plugin climate change

---
+++ Projects/ClimateChange/src/xyz/yetimountain/Yimura_Z4mbi3/Main.java
@@ -0,0 +1,90 @@
+package xyz.yetimountain.Yimura_Z4mbi3;
+
+import java.util.*;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener
+{
+  public static final String ANSI_RESET = "\u001B[0m"; 
+  public static final String ANSI_BLACK = "\u001B[30m"; 
+  public static final String ANSI_RED = "\u001B[31m"; 
+  public static final String ANSI_GREEN = "\u001B[32m"; 
+  public static final String ANSI_YELLOW = "\u001B[33m"; 
+  public static final String ANSI_BLUE = "\u001B[34m"; 
+  public static final String ANSI_PURPLE = "\u001B[35m"; 
+  public static final String ANSI_CYAN = "\u001B[36m"; 
+  public static final String ANSI_WHITE = "\u001B[37m";
+  
+  @Override
+  public void onEnable()
+  {
+    this.saveDefaultConfig();
+    //Save events
+    getServer().getPluginManager().registerEvents(this,this);
+    System.out.println(ANSI_GREEN + "Plugin enabled..");
+  }
+  
+  @Override
+  public void onDisable()
+  {
+    System.out.println(ANSI_RED + "Plugin shutting down...");
+  }
+  
+  @Override
+  public boolean onCommand(CommandSender sender, Command cmd, String Label, String[] args)
+  {
+    if(cmd.getName().equalsIgnoreCase("climate help")
+    {
+        sender.sendMessage(ChatColor.CYAN + "This plugin will track in which biome you're situated in.");
+        sender.sendMessage(ChatColor.CYAN + "Based on that, conditions will be added onto you.");
+        return true;
+    }
+    return false;
+  }
+  
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent event)
+  {
+      Player player = event.getPlayer();
+      Location loc = player.getLocation();
+      biome = loc.getWorld().getBiome(loc.getBlockX(), loc.getBlockZ);
+      double player_health = player.getHealthScale();
+ 
+      if(biome == "ICE_MOUNTAINS" || biome == "FROZEN_OCEAN" || biome == "FROZEN_RIVER" || biome == "ICE_FLATS")
+      {
+          Timer timer = new Timer();
+          timer.schedule(new TimerTask() 
+          {
+              int slow = this.getConfig().getInt("timers.slow");
+              @Override
+              public void run() 
+              {
+                  //Slowing of player after a set amount of time
+              }
+          }, slowtimer*60); //config time...
+          
+          timer.schedule(new TimerTask() 
+          {
+              int damage = this.getConfig().getInt("timers.damage");
+              @Override
+              public void run() 
+              {
+                  //Damaging of player after a set amount of time
+              }
+          }, damagetimer*60); //config time...
+      }
+      else if(biome == "DESSERT")
+      {
+          //TODO later
+      }
+   }
+}

5f16c178e14c018065f8cc2fd8746ba9f3e19bd6
Yimura <andreas.maerten@scarlet.be>
2017-11-01 02:21:39 +0100

    Small change
    Just a change to make the plugin look nicer

--- Projects/Test plugin/src/xyz/yetimountain/Z4mbi3/Welcome.java
+++ Projects/Test plugin/src/xyz/yetimountain/Z4mbi3/Welcome.java
@@ -29,7 +29,7 @@ public class Welcome extends JavaPlugin implements Listener{
 		Player player = event.getPlayer();
 		if(player.hasPlayedBefore())
 		{
-			event.setJoinMessage(ChatColor.GREEN + player.getName() + " has connected to the server !");
+			event.setJoinMessage(ChatColor.GREEN + player.getName() + " has connected to the server!");
 		}
 		else
 		{
